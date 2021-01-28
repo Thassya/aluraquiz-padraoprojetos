dacbeb6be90131c25d9d4cb30dcc8980e3011ce6
Thassya <thayowisky@gmail.com>
2021-01-28 21:53:48 +0100

    correção de tags

--- pages/_app.js
+++ pages/_app.js
@@ -39,14 +39,14 @@ export default function App({ Component, pageProps }) {
       <meta name="description" content={db.description} />
 
       {/* <!-- Open Graph / Facebook --> */}
-      <meta property="og:type" content="website">
+      <meta property="og:type" content="website" />
       <meta property="og:url" content={db.url} />
       <meta property="og:title" content={db.title} />
       <meta property="og:description" content={db.description} />
       <meta property="og:image" content={db.bg} />
 
       {/* <!-- Twitter --> */}
-      <meta property="twitter:card" content="summary_large_image">
+      <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={db.url} />
       <meta property="twitter:title" content={db.title} />
       <meta property="twitter:description" content={db.description} />
