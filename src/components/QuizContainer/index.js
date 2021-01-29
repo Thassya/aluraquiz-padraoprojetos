a617edc7c321e0792b2bdf047dd0982bd4acd752
Thassya <thayowisky@gmail.com>
2021-01-29 23:32:55 +0100

    ajustes finos

--- src/components/QuizContainer/index.js
+++ src/components/QuizContainer/index.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const QuizContainer = styled.div`
   width: 100%;
-  max-width: 333px;
+  max-width: 350px;
   padding-top: 45px;
   margin: auto 13%;
   @media screen and (max-width: 500px) {
